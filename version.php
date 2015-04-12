2a633722238858bdf7a83338bc560227556194ba
Ryan Nutt <ryan@nutt.com>
2015-04-12 11:43:33 -0500

    Forgot to update version;

--- version.php
+++ version.php
@@ -4,7 +4,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
  
-$plugin->version   = 2015041101;
+$plugin->version   = 2015041200;
 $plugin->requires  = 2014051200;
 $plugin->cron      = 0;
 $plugin->component = 'filter_responsivevideo';
